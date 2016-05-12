84043242bc04889ff31e832eff4f2ff6624519d6
hth1000th <tahwang@puc.edu>
2016-05-12 14:20:14 -0700

    modified server

--- server.rb
+++ server.rb
@@ -21,57 +21,82 @@ class Server
         end
         @user_list[username] = client
         client.puts "CONNECTED"
-        client.puts "If you want BROADCAST enter 1 or if you want SEND enter 2"
-        choice = client.gets.chomp
-        if choice == '1'
-          broadcast(username, client)
-        elsif choice == '2'
-          send(username, client)
-        else
-          client.puts "Wrong input!"
-        end
+        commands(username, client)
       end
     end
   end
 
-  def broadcast(username, client)
+  def commands(username, client)
     loop do
-      message = client.gets.chomp
-      if message[0] == 'e'
-        client.close
-      end
-      puts "BROADCAST #{message}"
-      client.puts "SENT"
-      @user_list.each do |other_username, other_client|
-        if other_client != client
-          other_client.puts "BROADCASTED #{username}: #{message}"
-          puts "RECEIVED"
+      command = client.gets.chomp
+      other_user = Array.new
+      message = Array.new
+      if command[0..9] == "broadcast "
+        message.push(*command[10..-1])
+        broadcast(username, client, message.join(""))
+      elsif command[0..4] == "send "
+        command[5..-1].split("").each do |c|
+          if c == " "
+            break
+          else
+            other_user.push(c)
+          end
         end
+        message.push(*command[5..-1])
+        message[0].slice!(0..other_user.length)
+        send(username, client, other_user.join(""), message.join(""))
+      elsif command[0..7] == "userlist"
+        userlist(username, client)
+      elsif command[0..9] == "disconnect"
+        disconnect(username, client)
+      else
+        client.puts "Wrong input!"
       end
     end
   end
 
-  def send(username, client)
-    loop do
-      client.puts "Enter the username to send the message:"
-      other_user = client.gets.chomp
-      client.puts "Enter the message:"
-      message = client.gets.chomp
-      if message[0] == 'e'
-        client.close
-      end
-      puts "SEND #{other_user} #{message}"
-      @user_list.each do |u, c|
-        if other_user == u && message != NIL
-          client.puts "SENT"
-          @user_list[other_user].puts "SENTFROM #{username} #{message}"
-          puts "RECEIVED"
-        end
+  def broadcast(username, client, message)
+    puts "BROADCAST #{message}"
+    client.puts "SENT"
+    @user_list.each do |other_username, other_client|
+      if other_client != client
+        other_client.puts "BROADCASTED #{username}: #{message}"
+        puts "RECEIVED"
       end
     end
   end
 
-  def disconnected
+  def send(username, client, other_user, message)
+    puts "SEND #{other_user} #{message}"
+    @user_list.each do |u, c|
+      if other_user == u && message != NIL
+        client.puts "SENT"
+        @user_list[other_user].puts "SENTFROM #{username} #{message}"
+        puts "RECEIVED"
+      end
+    end
+  end
+
+  def userlist(username, client)
+    @user_list.each do |u, c|
+      if username == u
+        client.puts "My username: #{u}"
+      else
+        client.puts "Other client: #{u}"
+      end
+    end
+  end
+
+  def disconnect(username, client)
+    puts "DISCONNECT"
+    client.puts "DISCONNECTED"
+    @user_list.each do |u, c|
+      if username != u
+        @user_list[u].puts "DISCONNECTED #{username}"
+        puts "RECEIVED"
+      end
+    end
+    client.close
   end
 
   def stopping
